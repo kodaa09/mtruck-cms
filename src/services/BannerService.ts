19db5ce40e0c86d00372b80c33eb33337a5f6f1b
JeremyElbaze <jeremy-elbaze@outlook.fr>
2023-01-24 14:51:47 +0100

    remove log

--- src/services/BannerService.ts
+++ src/services/BannerService.ts
@@ -1,5 +1,4 @@
 import { BannerType } from "@/types/BannerType";
-console.log(process.env.NEXT_PUBLIC_API_URL, "QKFNZLKN,MZL,GMLZ,FMLZ,FLMZ,;FLZNJKFNZKL FLKZ,NCKOZ,CKZ");
 
 class BannerService {
   static instance: BannerService;
@@ -24,7 +23,6 @@ class BannerService {
   async getBannerById(id: number) {
     let goodBanner: BannerType | undefined = undefined;
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    console.log(apiUrl);
     const res = await fetch(`${apiUrl}/api/banners?populate=*`);
     if (!res.ok) throw new Error(`Failed to fetch banner ${id} data`);
     const json = await res.json();
@@ -41,7 +39,6 @@ class BannerService {
     const res = await fetch(`${apiUrl}/api/banners?populate=*`);
     if (!res.ok) throw new Error(`Failed to fetch banner ${name} data`);
     const json = await res.json();
-    console.log({data: json?.data});
     const banners = json.data;
     banners.forEach((banner: BannerType) => {
       if (banner.attributes.name === name) goodBanner = banner;
